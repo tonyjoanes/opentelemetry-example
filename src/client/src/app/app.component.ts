609a5d198ca8625ca6f6af45c03761e827639e3e
Tony Joanes <tony.joanes@elekta.com>
2022-03-22 14:49:01 +0000

    working backend and front end with proxy

---
+++ src/client/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+interface WeatherForecast {
+        date: Date;
+        temperatureC: number;
+        temperatureF: number;
+        summary: string;
+    }
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client';
+  result: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) {
+    console.log('AppComponent constructor');
+    this.getData();
+  }
+
+  getData() {
+    console.log("Jafet")
+    this.http.get('/weatherforecast').subscribe((data) => {
+      console.log(data);
+      this.result = data as WeatherForecast[];
+    });
+  }
+
+}
